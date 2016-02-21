706beea319aaf9eb4b22298fa6864b4aeda42f05
Gantcho Kojuharov <gantcho@greyarch.net>
2016-02-21 17:23:25 +0100

    better command handling

--- lib/elixibot/slack.ex
+++ lib/elixibot/slack.ex
@@ -5,11 +5,19 @@ defmodule Elixibot.Slack do
 
   def start_link, do: start_link(@token, [])
 
-  def handle_message(message = %{type: "message", text: text}, slack, state) do
-    if text == "<@#{slack.me.id}>: mix" do
-      Slack.send_message("What's your poison, sir!", message.channel, slack)
+  def response(msg) do
+    [_, cmd] = msg
+    case cmd do
+      "mix" -> "What's your poison, sir?"
+      _ -> "Hmm, I am not sure I understand..."
     end
+  end
 
+  def handle_message(message = %{type: "message", text: text}, slack, state) do
+    match = Regex.run ~r/<@#{slack.me.id}>:?\s*(.*)/, text
+    if match do
+      Slack.send_message(response(match), message.channel, slack)
+    end
     {:ok, state}
   end
   def handle_message(_message, _slack, state), do: {:ok, state}
